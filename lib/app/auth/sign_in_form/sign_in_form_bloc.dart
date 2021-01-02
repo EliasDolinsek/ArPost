60b1680feed37a0b5cdb6110c7e30affff31b13b
Elias Dolinsek <elias.dolinsek@gmail.com>
2021-01-02 13:18:25 +0100

    Sign in form events and states

---
+++ lib/app/auth/sign_in_form/sign_in_form_bloc.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:ar_post/domain/auth/i_auth_facade.dart';
+import 'package:ar_post/domain/auth/auth_failure.dart';
+import 'package:ar_post/domain/auth/value_objects.dart';
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'sign_in_form_event.dart';
+part 'sign_in_form_state.dart';
+part 'sign_in_form_bloc.freezed.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade _authFacade;
+
+  SignInFormBloc(this._authFacade) : super(SignInFormState.initial());
+
+  @override
+  Stream<SignInFormState> mapEventToState(
+    SignInFormEvent event,
+  ) async* {}
+}
