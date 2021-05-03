a61ef4440fdad8df2f19bb9f7c0012faa6fbe591
eld <elias.dolinsek@gmail.com>
2021-05-03 18:26:46 +0200

    Add publish method to ICommentFacade

--- lib/domain/comments/i_comment_facade.dart
+++ lib/domain/comments/i_comment_facade.dart
@@ -1,3 +1,8 @@
-abstract class ICommentFacade {
+import 'package:ar_post/domain/auth/user.dart';
+import 'package:ar_post/domain/comments/comment_failure.dart';
+import 'package:ar_post/domain/core/value_objects.dart';
+import 'package:dartz/dartz.dart';
 
+abstract class ICommentFacade {
+  Future<Either<CommentFailure, Unit>> publishComment(UniqueId postId, User user, NonEmptyText postText);
 }
