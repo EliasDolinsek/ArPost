1571e83cea58479e0d9b86d65eb27e3d2ed085ab
eld <elias.dolinsek@gmail.com>
2021-05-10 21:29:30 +0200

    User email display in post details

--- lib/presentation/posts/post_details_page.dart
+++ lib/presentation/posts/post_details_page.dart
@@ -7,6 +7,7 @@ import 'package:ar_post/presentation/core/app_bar_widget.dart';
 import 'package:ar_post/presentation/posts/post_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PostDetailsPage extends StatelessWidget {
   final Post post;
@@ -38,12 +39,54 @@ class PostDetailsPage extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          PostImage(imageUrl: post.imageUrl),
+          Stack(
+            children: [
+              Material(
+                elevation: 3,
+                child: PostImage(imageUrl: post.imageUrl),
+              ),
+              _buildPostDetails(),
+            ],
+          ),
           CommentsWidget(
             postId: post.id,
             user: user,
           )
         ],
+      ),
+    );
+  }
+
+  Widget _buildPostDetails() {
+    return SizedBox(
+      width: double.infinity,
+      height: 500,
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 10.0,
+            bottom: 10.0,
+            right: 10.0,
+          ),
+          child: Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Text(
+                    post.userEmail,
+                    style: GoogleFonts.openSans(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
